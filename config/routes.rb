f49f558e7c633c4a89884ae4aab11225dd1ee3af
seanbman <seannicholasburman@gmail.com>
2026-09-04 20:31:46 -0600

    Add development debug ingest route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  post "debug_ingest" => "debug_logs#create"
+
   root "instrument#show"
 end
