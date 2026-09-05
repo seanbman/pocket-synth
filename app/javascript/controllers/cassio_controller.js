d0b0a2762f5a3214f621d19ebb0deaa0f50f6ee7
seanbman <seannicholasburman@gmail.com>
2026-09-04 22:36:53 -0600

    Install track naming runtime

--- app/javascript/controllers/cassio_controller.js
+++ app/javascript/controllers/cassio_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { CassioApp } from "cassio/app"
 import { installDeepAudioTrace } from "cassio/debug_audio_hooks"
 import { installRecordingRuntime } from "cassio/recording_runtime"
+import { installTrackNamingRuntime } from "cassio/track_naming_runtime"
 import { installTrackPatternRuntime } from "cassio/track_pattern_runtime"
 import {
   audioSnapshot,
@@ -19,6 +20,7 @@ export default class extends Controller {
     try {
       this.app = new CassioApp(this.element)
       installTrackPatternRuntime(this.app)
+      installTrackNamingRuntime(this.app)
       installRecordingRuntime(this.app)
       installAudioTrace(this.app)
       installDeepAudioTrace(this.app)
